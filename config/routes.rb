ca87842d672bfa9eae5c20b31a449b06cd33c8f6
takataka06 <206020787+takataka06@users.noreply.github.com>
2025-06-11 11:28:45 +0000

    proceed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  resources :books, only: [:index,:show,:edit,:update,:destroy]
   devise_for :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
