f281d31fbad89b448701de644952db5e6e54fa6f
takataka06 <206020787+takataka06@users.noreply.github.com>
2025-06-13 14:44:11 +0000

    Finish part

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,24 +5,46 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book =Book.find(params[:id])
+    @new_book =Book.new
   end
   def edit
   end
   def update
-  end
-  def destroy
-  end
-  def create
-    @book =Book.new(book_params)
-    @book.user_id = current_user
-    if @book.save
-      redirect_to books_path,notice: "Book created!"
+    @book =Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] ="Update succesfully"
+      redirect_to @book
     else
-      @books = Book.all
-      flash.now[:alert] = "保存に失敗しました"
+      @books =Book.all
+      flash.now[:notice] ="Update error!"
       render :index
     end
   end
+  def destroy
+    @book =Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] ="Delete succesfully"
+      redirect_to books_path
+    else
+      @books =Book.all
+      flash.now[:notice] ="Delete error!"
+      render :index
+    end
+  end
+  def create
+    @book =Book.new(book_params)
+    @book.user = current_user
+    if @book.save
+      redirect_to books_path,notice: "successfully created!"
+    else
+      @books = Book.all
+      flash.now[:alert] = "happen error"
+      render :index
+    end
+  end
+  private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
