910d70f75af0b125572a9a84e2bc7494e3fc09b2
takataka06 <206020787+takataka06@users.noreply.github.com>
2025-06-13 04:10:19 +0000

    Finish bookindex

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   def index
+    @books =Book.all
+    @book =Book.new
   end
 
   def show
@@ -11,5 +13,17 @@ class BooksController < ApplicationController
   def destroy
   end
   def create
+    @book =Book.new(book_params)
+    @book.user_id = current_user
+    if @book.save
+      redirect_to books_path,notice: "Book created!"
+    else
+      @books = Book.all
+      flash.now[:alert] = "保存に失敗しました"
+      render :index
+    end
+  end
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,6 @@ class UsersController < ApplicationController
   def index
     @users =User.all
   end
+
+  d
 end
